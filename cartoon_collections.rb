037ee696471574ea4785b901276aff5cceb6cc6e
arnie b <arnold.bwaila@gmail.com>
2019-08-24 20:30:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 def summon_captain_planet(array)# code an argument here
   # Your code here
   cnt = 0
-  newarray
+  newarray = []
   array.map do |elmnt|
     newarray << elmnt.capitalize +"!"
   end
