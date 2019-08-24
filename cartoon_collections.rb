ec23316b42e75011f7aaacd7bce0005cc0cdd0af
arnie b <arnold.bwaila@gmail.com>
2019-08-24 20:42:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,12 @@ def summon_captain_planet(array)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) code an argument here
   # Your code here
+  array.all? do |elmnt|
+    elmnt.length > 4
+  end
+  
 end
 
 def find_the_cheese# code an argument here
