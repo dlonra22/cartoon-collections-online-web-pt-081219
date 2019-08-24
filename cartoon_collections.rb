14bd99b26ea705d6863997ff544fa77c0d2de617
arnie b <arnold.bwaila@gmail.com>
2019-08-24 20:49:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def summon_captain_planet(array) # code an argument here
 end
 
 def long_planeteer_calls(array) code an argument here
-  # Your code here
   array.any? do |elmnt|
     elmnt.size > 4
   end
